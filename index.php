ba0a3a03a638ae137610b28b6d1cda5f28a444b5
Wenqiang Bo <bowenqiang@live.com>
2017-02-28 15:10:17 -0600

    update

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Your Daily Menu</title>
+
+    <!-- Bootstap-->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+
+    <!-- Custom styles for this template -->
+    <link href="css/navbar-fixed-footer-sticky.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+  	<?php include('template/navigation.php'); ?>
+
+
+
+
+    <div class="container">
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <h1>Navbar example</h1>
+        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+        <p>To see the difference between static and fixed top navbars, just scroll.</p>
+        <p>
+          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
+        </p>
+      </div>
+
+    </div> <!-- /container -->
+    <?php include('template/footer.php') ?>
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+
+include('template/googleLogin.php');
+
+//Display user info or display login url as per the info we have.
+echo '<div style="margin:20px">';
+if (isset($authUrl)){ 
+	//show login url
+	echo '<div align="center">';
+	echo '<h3>Login with Google -- Demo</h3>';
+	echo '<div>Please click login button to connect to Google.</div>';
+	echo '<a class="login" href="' . $authUrl . '"><img src="images/google-login-button.png" /></a>';
+	echo '</div>';
+	
+} else {
+	
+	$user = $service->userinfo->get(); //get user info 	
+	//show user picture
+	echo '<img src="'.$user->picture.'" style="float: right;margin-top: 33px;" />';
+	//print user details
+	echo '<pre>';
+	print_r($user);
+	echo '</pre>';
+}
+echo '</div>';
+
+
+?>
