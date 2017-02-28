0eded659efc02bd5904a7930b7c8dd92e05e89f8
Wenqiang Bo <bowenqiang@live.com>
2017-02-28 16:57:19 -0600

    add logout

--- template/navigation.php
+++ template/navigation.php
@@ -31,7 +31,7 @@
                 <li role="separator" class="divider"></li>
                 <li class="dropdown-header">Nav header</li>
                 <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
+                <li><a href="logout.php">Logout</a></li>
               </ul>
             </li>
               <?php }
