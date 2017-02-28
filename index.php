5d58f2b527fde142fc0dca3d515de77b315b21f2
Wenqiang Bo <bowenqiang@live.com>
2017-02-28 16:54:57 -0600

    fix missing ;

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php 
 session_start();
 if(isset($_SESSION['access_token'])) {
-	    $fname = $_SESSION['firstName']
-	    $lname = $_SESSION['lastName'] 
-	    $userId = $_SESSION['userID']
-	    $userImage = $_SESSION['imgURL'] 
-	    $userEmail = $_SESSION['email']
+	    $fname = $_SESSION['firstName'];
+	    $lname = $_SESSION['lastName'] ;
+	    $userId = $_SESSION['userID'];
+	    $userImage = $_SESSION['imgURL'] ;
+	    $userEmail = $_SESSION['email'];
 	}
 ?>
 <!DOCTYPE html>
